d42440ea8e04f223510e31ec160d8ccfe54bfe19
smiwayaki0820 <lab.skow.mywk@gmail.com>
2020-05-23 22:28:17 +0900

    add run/train .sh

---
+++ run.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/bash -eu
+
+set -eux
+
+USAGE="Usage: bash run.sh -g [GPU_ID] -m [MODE]"
+
+while getopts g:m:o: OPT
+do
+  case ${OPT} in
+    "g" ) FLG_G="TRUE"; GPU_ID=${OPTARG};;
+    "m" ) FLG_M="TRUE"; MODE=${OPTARG};;
+    "o" ) FLG_O="TRUE"; SAVE_DIR=${OPTARG};;
+    * ) echo ${USAGE} 1>&2
+        exit 1 ;;
+  esac
+done
+
+if test "${GPU_ID}" != "TRUE"; then
+  GPU_ID=0
+elif test "${MODE}" != "TRUE"; then
+  echo "SELECT MODE ... train/test/both"
+  echo ${USAGE} 1>&2
+  exit 1
+fi
+
+
+# run ==============================================
+
+bash config.sh
+
+cat <<EOS > /tmp/work.txt
+
+ARCH=transformer
+SEED=0
+EPOCH=20
+BATCH=64
+M_TOKENS=3000
+LOG_INTVL=1000
+PRETRAIN=False
+
+LR=0.001
+LR_SCHEDULER=triangular
+M_LR=0.004
+LR_UPDATE=73328
+LR_SHRINK=0.95
+CLIP=2
+
+DOUT=0.2
+RELU_DOUT=0.2
+ATT_DOUT=0.2
+CP_ATT_DOUT=0.2
+
+ENC_EDIM=512
+M_SRC_POS=1024
+ENC_FFN_EDIM=4096
+ENC_ATT_HEADS=8
+
+DEC_EDIM=512
+M_TGT_POS=1024
+DEC_FFN_EDIM=4096
+DEC_ATT_HEADS=8
+
+CP_ATT_HEADS=1
+POS_LBL_W=1.2
+
+EOS
+
+
+if [ ${MODE} = 'train' ] || [ ${MODE} = 'both' ] ; then 
+  echo 'TRAIN'
+  bash train/srl_train.sh
+  cp /tmp/work.txt ${OUT}/experiment.config
+  echo 'FIN TRAIN'
+
+else
+  echo 'invalid MODE'
+
+fi
+
