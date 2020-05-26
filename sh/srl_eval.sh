edb4452cc4827b1fbeb95cd6d0a19f3c7c3324d1
smiwayaki0820 <lab.skow.mywk@gmail.com>
2020-05-27 06:23:22 +0900

    update sh/srl_eval.sh

--- sh/srl_eval.sh
+++ sh/srl_eval.sh
@@ -2,43 +2,55 @@
 # copy from generate.sh
 
 source ./config.sh
+. /tmp/work.txt
 
 set -e
 
-ema='ema'
+EMA=''
 
-rm -rf $DATA_RAW/test.src-tgt.src $DATA_RAW/test.src-tgt.tgt
-python gec_scripts/split.py $DATA_RAW/test.src-tgt.src.old $DATA_RAW/test.src-tgt.src $DATA_RAW/test.idx
-cp $DATA_RAW/test.src-tgt.src $DATA_RAW/test.src-tgt.tgt
+rm -rf ${DATA_RAW}/test.src-tgt.src
+rm -rf ${DATA_RAW}/test.src-tgt.tgt
 
-epochs='_last'
-for epoch in ${epochs[*]}; do
-    if [ -f $RESULT/m2score$ema$exp_$epoch.log ]; then
+
+### gec_scripts/split.py ###
+python gec_scripts/split.py \
+  ${DATA_RAW}/test.src-tgt.src.old \
+  ${DATA_RAW}/test.src-tgt.src \
+  ${DATA_RAW}/test.idx
+
+cp ${DATA_RAW}/test.src-tgt.src ${DATA_RAW}/test.src-tgt.tgt
+
+### generate.py ###
+EPOCHS=${EPOCHS}
+for EPOCH in ${EPOCHS[*]}; do
+    if [ -f ${RESULT}/m2score${EMA}${exp}_${EPOCH}.log ]; then
+        echo "NotFound ... ${RESULT}/m2score${EMA}${exp}_${EPOCH}.log"
         continue
     fi
-    echo $epoch
+    echo ${EPOCH}
 
-    CUDA_VISIBLE_DEVICES=$device python generate.py $DATA_RAW \
-    --path $MODELS/checkpoint$ema$epoch.pt \
-    --beam 12 \
-    --nbest 12 \
+    CUDA_VISIBLE_DEVICES=${GPU_ID} python generate.py $DATA_RAW \
+    --path ${MODELS}/checkpoint${EMA}${EPOCH}.pt \
+    --beam ${BEAM} \
+    --nbest ${NBEST} \
     --gen-subset test \
-    --max-tokens 6000 \
+    --max-tokens ${M_TOKENS_} \
     --no-progress-bar \
     --raw-text \
-    --batch-size 128 \
+    --batch-size ${BATCH} \
     --print-alignment \
-    --max-len-a 0 \
+    --max-len-a ${M_LEN_A} \
     --no-early-stop \
     --copy-ext-dict --replace-unk \
-    > $RESULT/output$ema$epoch.nbest.txt 
+    > ${RESULT}/output${EMA}${EPOCH}.nbest.txt 
 
-    cat $RESULT/output$ema$epoch.nbest.txt | grep "^H" | python ./gec_scripts/sort.py 12 $RESULT/output$ema$epoch.txt.split
+    cat ${RESULT}/output${EMA}${EPOCH}.nbest.txt | grep "^H" | python ./gec_scripts/sort.py 12 ${RESULT}/output${EMA}${EPOCH}.txt.split
 
-    python ./gec_scripts/revert_split.py $RESULT/output$ema$epoch.txt.split $DATA_RAW/test.idx > $RESULT/output$ema$epoch.txt
+    ## gec_scripts/revert_split.py ##
+    python ./gec_scripts/revert_split.py ${RESULT}/output${EMA}${EPOCH}.txt.split $DATA_RAW/test.idx > ${RESULT}/output${EMA}${EPOCH}.txt
 
-    python2 ./gec_scripts/m2scorer/m2scorer -v $RESULT/output$ema$epoch.txt ./data/test.m2 > $RESULT/m2score$ema$exp_$epoch.log
-    tail -n 1 $RESULT/m2score$ema$exp_$epoch.log
+    python2 ./gec_scripts/m2scorer/m2scorer -v ${RESULT}/output${EMA}${EPOCH}.txt ./data/test.m2 > ${RESULT}/m2score${EMA}${exp}_${EPOCH}.log
+    tail -n 1 ${RESULT}/m2score${EMA}${exp}_${EPOCH}.log
 done
 
-python gec_scripts/show_m2.py $RESULT/m2score$ema$exp_{}.log
+python gec_scripts/show_m2.py ${RESULT}/m2score${EMA}${exp}_{}.log
